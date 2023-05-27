1ab65828167cd0c4feb0673765de8007a8048e96
SonjoyRoy1907073 <85251515+SonjoyRoy1907073@users.noreply.github.com>
2023-05-27 17:20:43 +0600

    ddl dml

---
+++ plsql.sql
@@ -0,0 +1,15 @@
+--declare 
+--<declarations section>
+--begin
+--<execute commands>
+--exception
+--exception handling
+--end;
+DECLARE
+ message VARCHAR(69):='Furniture management system';
+BEGIN
+ DBMS_OUTPUT.put_line(message);
+END;
+/
+
+
